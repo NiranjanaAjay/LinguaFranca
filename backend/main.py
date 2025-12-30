efe112185a8a41b75d7b67d0c486f5db02006fe0
rose <rosefrancisrose16@gmail.com>
2025-12-30 12:29:36 +0530

    Fix: remove extra slash

--- backend/main.py
+++ backend/main.py
@@ -36,7 +36,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://lingua-franca-frontend.vercel.app/"],
+    allow_origins=["https://lingua-franca-frontend.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
